23f1615e4c50601ae0ab148cf01e8ae1bbc9d3ec
hayarov <edurda77@gmail.com>
2024-03-19 11:44:57 +0500

    v1_2

---
+++ app/src/main/java/one/win/casino/rt145/domain/model/Task2Rt145.kt
@@ -0,0 +1,438 @@
+package one.win.casino.rt145.domain.model
+
+import one.win.casino.rt145.R
+
+
+val quizHokkey = listOf(
+    QuizTaskRt145(
+        id = 1,
+        question = R.string.h_quest_1,
+        answers = listOf(
+            R.string.h_response_1_1,
+            R.string.h_response_1_2,
+            R.string.h_response_1_3,
+            R.string.h_response_1_4
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 2,
+        question = R.string.h_quest_2,
+        answers = listOf(
+            R.string.h_response_2_1,
+            R.string.h_response_2_2,
+            R.string.h_response_2_3,
+            R.string.h_response_2_4
+        ),
+        correctAnswerNum = 1
+    ),
+    QuizTaskRt145(
+        id = 3,
+        question = R.string.h_quest_3,
+        answers = listOf(
+            R.string.h_response_3_1,
+            R.string.h_response_3_2,
+            R.string.h_response_3_3,
+            R.string.h_response_3_4
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 4,
+        question = R.string.h_quest_4,
+        answers = listOf(
+            R.string.h_response_4_1,
+            R.string.h_response_4_2,
+            R.string.h_response_4_3,
+            R.string.h_response_4_4
+        ),
+        correctAnswerNum = 1
+    ),
+    QuizTaskRt145(
+        id = 5,
+        question = R.string.h_quest_5,
+        answers = listOf(
+            R.string.h_response_5_1,
+            R.string.h_response_5_2,
+            R.string.h_response_5_3,
+            R.string.h_response_5_4
+        ),
+        correctAnswerNum = 1
+    ),
+    QuizTaskRt145(
+        id = 6,
+        question = R.string.h_quest_6,
+        answers = listOf(
+            R.string.h_response_6_1,
+            R.string.h_response_6_2,
+            R.string.h_response_6_3,
+            R.string.h_response_6_4
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 7,
+        question = R.string.h_quest_7,
+        answers = listOf(
+            R.string.h_response_7_1,
+            R.string.h_response_7_2,
+            R.string.h_response_7_3,
+            R.string.h_response_7_4
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 8,
+        question = R.string.h_quest_8,
+        answers = listOf(
+            R.string.h_response_8_1,
+            R.string.h_response_8_2,
+            R.string.h_response_8_3,
+            R.string.h_response_8_4
+        ),
+        correctAnswerNum = 1
+    ),
+    QuizTaskRt145(
+        id = 9,
+        question = R.string.h_quest_9,
+        answers = listOf(
+            R.string.h_response_9_1,
+            R.string.h_response_9_2,
+            R.string.h_response_9_3,
+            R.string.h_response_9_4
+        ),
+        correctAnswerNum = 4
+    ),
+    QuizTaskRt145(
+        id = 10,
+        question = R.string.h_quest_10,
+        answers = listOf(
+            R.string.h_response_10_1,
+            R.string.h_response_10_2,
+            R.string.h_response_10_3,
+            R.string.h_response_10_4
+        ),
+        correctAnswerNum = 1
+    ),
+)
+
+val quizVolleyBall = listOf(
+    QuizTaskRt145(
+        id = 1,
+        question = R.string.v_quest_1,
+        answers = listOf(
+            R.string.v_response_1_1,
+            R.string.v_response_1_2,
+            R.string.v_response_1_3,
+            R.string.v_response_1_4
+        ),
+        correctAnswerNum = 4
+    ),
+    QuizTaskRt145(
+        id = 2,
+        question = R.string.v_quest_2,
+        answers = listOf(
+            R.string.v_response_2_1,
+            R.string.v_response_2_2,
+            R.string.v_response_2_3,
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 3,
+        question = R.string.v_quest_3,
+        answers = listOf(
+            R.string.v_response_3_1,
+            R.string.v_response_3_2,
+            R.string.v_response_3_3,
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 4,
+        question = R.string.v_quest_4,
+        answers = listOf(
+            R.string.v_response_4_1,
+            R.string.v_response_4_2,
+            R.string.v_response_4_3,
+        ),
+        correctAnswerNum = 1
+    ),
+    QuizTaskRt145(
+        id = 5,
+        question = R.string.v_quest_5,
+        answers = listOf(
+            R.string.v_response_5_1,
+            R.string.v_response_5_2,
+            R.string.v_response_5_3,
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 6,
+        question = R.string.v_quest_6,
+        answers = listOf(
+            R.string.v_response_6_1,
+            R.string.v_response_6_2,
+            R.string.v_response_6_3,
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 7,
+        question = R.string.v_quest_7,
+        answers = listOf(
+            R.string.v_response_7_1,
+            R.string.v_response_7_2,
+            R.string.v_response_7_3,
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 8,
+        question = R.string.v_quest_8,
+        answers = listOf(
+            R.string.v_response_8_1,
+            R.string.v_response_8_2,
+            R.string.v_response_8_3,
+        ),
+        correctAnswerNum = 1
+    ),
+    QuizTaskRt145(
+        id = 9,
+        question = R.string.v_quest_9,
+        answers = listOf(
+            R.string.v_response_9_1,
+            R.string.v_response_9_2,
+            R.string.v_response_9_3,
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 10,
+        question = R.string.v_quest_10,
+        answers = listOf(
+            R.string.v_response_10_1,
+            R.string.v_response_10_2,
+            R.string.v_response_10_3,
+        ),
+        correctAnswerNum = 2
+    ),
+)
+
+
+val quizVolleyTennis = listOf(
+    QuizTaskRt145(
+        id = 1,
+        question = R.string.t_quest_1,
+        answers = listOf(
+            R.string.t_response_1_1,
+            R.string.t_response_1_2,
+            R.string.t_response_1_3,
+            R.string.t_response_1_4
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 2,
+        question = R.string.t_quest_2,
+        answers = listOf(
+            R.string.t_response_2_1,
+            R.string.t_response_2_2,
+            R.string.t_response_2_3,
+            R.string.t_response_2_4
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 3,
+        question = R.string.t_quest_3,
+        answers = listOf(
+            R.string.t_response_3_1,
+            R.string.t_response_3_2,
+            R.string.t_response_3_3,
+            R.string.t_response_3_4
+        ),
+        correctAnswerNum = 4
+    ),
+    QuizTaskRt145(
+        id = 4,
+        question = R.string.t_quest_4,
+        answers = listOf(
+            R.string.t_response_4_1,
+            R.string.t_response_4_2,
+            R.string.t_response_4_3,
+            R.string.t_response_4_4
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 5,
+        question = R.string.t_quest_5,
+        answers = listOf(
+            R.string.t_response_5_1,
+            R.string.t_response_5_2,
+            R.string.t_response_5_3,
+            R.string.t_response_5_4
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 6,
+        question = R.string.t_quest_6,
+        answers = listOf(
+            R.string.t_response_6_1,
+            R.string.t_response_6_2,
+            R.string.t_response_6_3,
+            R.string.t_response_6_4
+        ),
+        correctAnswerNum = 4
+    ),
+    QuizTaskRt145(
+        id = 7,
+        question = R.string.t_quest_7,
+        answers = listOf(
+            R.string.t_response_7_1,
+            R.string.t_response_7_2,
+            R.string.t_response_7_3,
+            R.string.t_response_7_4
+        ),
+        correctAnswerNum = 4
+    ),
+    QuizTaskRt145(
+        id = 8,
+        question = R.string.t_quest_8,
+        answers = listOf(
+            R.string.t_response_8_1,
+            R.string.t_response_8_2,
+            R.string.t_response_8_3,
+            R.string.t_response_8_4
+        ),
+        correctAnswerNum = 1
+    ),
+    QuizTaskRt145(
+        id = 9,
+        question = R.string.t_quest_9,
+        answers = listOf(
+            R.string.t_response_9_1,
+            R.string.t_response_9_2,
+            R.string.t_response_9_3,
+            R.string.t_response_9_4
+        ),
+        correctAnswerNum = 4
+    ),
+    QuizTaskRt145(
+        id = 10,
+        question = R.string.t_quest_10,
+        answers = listOf(
+            R.string.t_response_10_1,
+            R.string.t_response_10_2,
+            R.string.t_response_10_3,
+            R.string.t_response_10_4
+        ),
+        correctAnswerNum = 2
+    ),
+)
+
+val quizBoxing = listOf(
+    QuizTaskRt145(
+        id = 1,
+        question = R.string.bx_quest_1,
+        answers = listOf(
+            R.string.bx_response_1_1,
+            R.string.bx_response_1_2,
+            R.string.bx_response_1_3,
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 2,
+        question = R.string.bx_quest_2,
+        answers = listOf(
+            R.string.bx_response_2_1,
+            R.string.bx_response_2_2,
+            R.string.bx_response_2_3,
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 3,
+        question = R.string.bx_quest_3,
+        answers = listOf(
+            R.string.bx_response_3_1,
+            R.string.bx_response_3_2,
+            R.string.bx_response_3_3,
+        ),
+        correctAnswerNum = 3
+    ),
+    QuizTaskRt145(
+        id = 4,
+        question = R.string.bx_quest_4,
+        answers = listOf(
+            R.string.bx_response_4_1,
+            R.string.bx_response_4_2,
+            R.string.bx_response_4_3,
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 5,
+        question = R.string.bx_quest_5,
+        answers = listOf(
+            R.string.bx_response_5_1,
+            R.string.bx_response_5_2,
+            R.string.bx_response_5_3,
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 6,
+        question = R.string.bx_quest_6,
+        answers = listOf(
+            R.string.bx_response_6_1,
+            R.string.bx_response_6_2,
+            R.string.bx_response_6_3,
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 7,
+        question = R.string.bx_quest_7,
+        answers = listOf(
+            R.string.bx_response_7_1,
+            R.string.bx_response_7_2,
+            R.string.bx_response_7_3,
+        ),
+        correctAnswerNum = 1
+    ),
+    QuizTaskRt145(
+        id = 8,
+        question = R.string.bx_quest_8,
+        answers = listOf(
+            R.string.bx_response_8_1,
+            R.string.bx_response_8_2,
+            R.string.bx_response_8_3,
+        ),
+        correctAnswerNum = 1
+    ),
+    QuizTaskRt145(
+        id = 9,
+        question = R.string.bx_quest_9,
+        answers = listOf(
+            R.string.bx_response_9_1,
+            R.string.bx_response_9_2,
+            R.string.bx_response_9_3,
+        ),
+        correctAnswerNum = 2
+    ),
+    QuizTaskRt145(
+        id = 10,
+        question = R.string.bx_quest_10,
+        answers = listOf(
+            R.string.bx_response_10_1,
+            R.string.bx_response_10_2,
+            R.string.bx_response_10_3,
+        ),
+        correctAnswerNum = 3
+    ),
+)
