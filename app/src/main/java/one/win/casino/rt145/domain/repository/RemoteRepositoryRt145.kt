9342b24d7a2f541804fc8b8cee3de095de373809
hayarov <edurda77@gmail.com>
2024-03-25 16:13:43 +0500

    v1_25

--- app/src/main/java/one/win/casino/rt145/domain/repository/RemoteRepositoryRt145.kt
+++ app/src/main/java/one/win/casino/rt145/domain/repository/RemoteRepositoryRt145.kt
@@ -1,15 +1,33 @@
 package one.win.casino.rt145.domain.repository
 
+import android.util.Log
 import one.win.casino.rt145.domain.model.GameDataRt145
 import one.win.casino.rt145.domain.utils.ResourceRt145
 
 interface RemoteRepositoryRt145 {
-    suspend fun sdgetFootballData(): ResourceRt145<List<GameDataRt145>>
-    suspend fun vftGetIceHockeyData(): ResourceRt145<List<GameDataRt145>>
-    suspend fun klmVtBasketballData(): ResourceRt145<List<GameDataRt145>>
-    suspend fun getUrlRt145(): ResourceRt145<String>
-    suspend fun getSharedUrlRt145(): String?
-    suspend fun setSharedUrlRt145(date: String)
-    suspend fun getFirstRt145(): Boolean
-    suspend fun setFirstRt145(date: Boolean)
+    suspend fun sdgetFootballData(): ResourceRt145<List<GameDataRt145>>  {
+        return ResourceRt145.Success(listOf())
+    }
+    suspend fun vftGetIceHockeyData(): ResourceRt145<List<GameDataRt145>> {
+        return ResourceRt145.Success(listOf())
+    }
+    suspend fun klmVtBasketballData(): ResourceRt145<List<GameDataRt145>> {
+        return ResourceRt145.Success(listOf())
+    }
+    suspend fun getUrlRt145(): ResourceRt145<String> {
+        return ResourceRt145.Success("")
+    }
+    suspend fun getSharedUrlRt145(): String? {
+        return "hh"
+    }
+    suspend fun setSharedUrlRt145(date: String) {
+        println("setSharedUrlRt14ff544")
+    }
+    suspend fun getFirstRt145(): Boolean {
+        println("setSharedUrlRt1ff4544")
+        return true
+    }
+    suspend fun setFirstRt145(date: Boolean) {
+        println("setSharedUrlRt14ff544")
+    }
 }
